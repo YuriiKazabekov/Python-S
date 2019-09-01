17ed49741e1d76b0b13631efbe30c23ef6051cbf
Yurii <be4ka1291@gmail.com>
2019-09-01 18:43:32 +0300

    4.6 done

--- Code_for Lab 4_x/Lab_4_5_generators.py
+++
@@ -1,9 +0,0 @@
-foo = [1, 2, 3, 4, 5]
-
-def my_enumerate(lst):   # Define own enumerate function
-	for item in lst:     # Loop for generation objects with yield
-		yield lst.index(item)+1, item  # Return tuple with 2 items - item index +1 and item value
-
-		
-for item in my_enumerate(foo):
-	print(item)
---
+++ SampleRepository/Defs/definitions.py
@@ -0,0 +1,8 @@
+from main import *
+pi = 3.14
+
+def functionOne():
+    print("function one call!")
+
+def functionTwo(arg):
+    print("function two call with arg:{}".format(arg))
